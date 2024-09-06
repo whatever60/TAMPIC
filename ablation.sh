abe5fc3a7ac305e17fa94cc9c635ee909f6c24df
whatever60 <quym@foxmail.com>
2024-09-06 04:38:20 +0000

    added ablation script

---
+++ ablation.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Path to the directory where the config files are located
+config_dir="ablation_configs/crop_size"
+
+# List of RGB crop sizes
+# declare -a rgb_sizes=("64" "96" "128" "192" "256" "384" "512")
+declare -a rgb_sizes=("64")
+
+# Loop through each config and execute train.py
+for size in "${rgb_sizes[@]}"
+do
+    config_file="${config_dir}/${size}.json"
+    echo "Running training with config ${config_file}..."
+    python train.py --config ${config_file} --name "ablation_crop-size_${size}"
+done
